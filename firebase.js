8e45e049ca8d56758dff1d103c769d467fdd2949
kutschera19 <57454086+kutschera19@users.noreply.github.com>
2026-03-29 15:30:25 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
 rules_version = '2';
 service cloud.firestore {
   match /databases/{database}/documents {
