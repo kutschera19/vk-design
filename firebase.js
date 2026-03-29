721290d55192add72d2a43595dc1df2a5e420567
kutschera19 <57454086+kutschera19@users.noreply.github.com>
2026-03-29 15:42:02 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+console.log ("FIREBASE CARREGOU");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
