3b93e95553176a38c26becac5471cbbd562cf32a
kutschera19 <57454086+kutschera19@users.noreply.github.com>
2026-03-29 15:31:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 import { db } from "./firebase.js";
 import { addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+async function testeFirebase() {
+  await addDoc(collection(db, "orders"), {
+    service: "Teste VK",
+    status: "pendente",
+    price: 50,
+    created_at: new Date()
+  });
+
+  alert("Firebase funcionando!");
+}
+
+window.testeFirebase = testeFirebase;
